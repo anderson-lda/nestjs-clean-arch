190401d66a46c557866cd7dae6893f59546e1e07
anderson-lda <anderson.lima.araujo@hotmail.com>
2024-10-04 20:54:34 -0300

    added update password integration tests

--- src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
@@ -123,4 +123,23 @@ describe("UserEntity integration tests",()=>{
       entity.update('other name')
     })
   })
+
+  describe("UpdatePassword method",()=>{
+    it("Should an invalid user using password field",()=>{
+      const entity = new UserEntity(userDataBuilder({}))
+      expect(()=>entity.updatePassword(null)).toThrow(EntityValidationError)
+      expect(()=>entity.updatePassword('')).toThrow(EntityValidationError)
+      expect(()=>entity.updatePassword(10 as any)).toThrow(EntityValidationError)
+      expect(()=>entity.updatePassword('a'.repeat(101))).toThrow(EntityValidationError)
+    })
+
+    it("Should a valid user",()=>{
+      expect.assertions(0)
+      const props: UserProps = {
+        ...userDataBuilder({}),
+      }
+      const entity = new UserEntity(props)
+      entity.updatePassword('otherpassword')
+    })
+  })
 })
