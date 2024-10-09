9bd1f8f882c40d9df04aff2c99030873d0a0cc82
anderson-lda <anderson.lima.araujo@hotmail.com>
2024-10-09 15:46:27 -0300

    inmemoryrepository unit tests for update method

--- src/shared/repositories/__tests__/unit/in-memory.repository.spec.ts
+++ src/shared/repositories/__tests__/unit/in-memory.repository.spec.ts
@@ -43,4 +43,20 @@ describe("InMemoryRepository unit tests", () => {
     const result = await sut.findAll()
     await expect([entity]).toStrictEqual(result)
   })
+
+  it("Should throw error on update when entity not found", async ()=>{
+    const entity = new StubEntity({name: 'test name', price: 50})
+    //no sut não existe pq não há o insert
+    await expect(sut.update(entity)).rejects.toThrow(
+      new NotFoundError('entity not found')
+    )
+  })
+
+  it("Should update an entity", async ()=>{
+    const entity = new StubEntity({name: 'test name', price: 50})
+    await sut.insert(entity)
+    const entityUpdated = new StubEntity({name: 'name updated', price: 10 }, entity._id,)
+    await sut.update(entityUpdated)
+    await expect(entityUpdated.toJSON()).toStrictEqual(sut.items[0].toJSON())
+  })
 })
