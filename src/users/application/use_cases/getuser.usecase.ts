5413dbc13990d48ad643fb215e0ed26d945f055c
anderson-lda <anderson.lima.araujo@hotmail.com>
2024-10-28 14:26:47 -0300

    added getuserusecase

---
+++ src/users/application/use_cases/getuser.usecase.ts
@@ -0,0 +1,26 @@
+import { UserRepository } from "@/users/domain/repositories/user.repository"
+
+export namespace GetUserUseCase {
+  export type Input = {
+    id: string
+  }
+
+  export type Output = {
+    name: string
+    email: string
+    password: string
+    id: string
+    createdAt: Date
+  }
+
+  export class UseCase {
+    constructor(
+      private userRepository: UserRepository.Repository,
+    ){}
+
+    async execute(input: Input): Promise<Output> {
+      const entity = await this.userRepository.findById(input.id)
+      return entity.toJSON()
+    }
+  }
+}
