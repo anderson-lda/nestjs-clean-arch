43840792f9b0aaba67a2a3739b5b01716522480a
anderson-lda <anderson.lima.araujo@hotmail.com>
2024-10-28 21:27:43 -0300

    added listusers usecase part 2

--- src/users/application/dto/user-output.ts
+++ src/users/application/dto/user-output.ts
@@ -1,7 +1,15 @@
+import { UserEntity } from "@/users/domain/entities/user.entity"
+
 export type UserOutput = {
   name: string
   email: string
   password: string
   id: string
   createdAt: Date
+}
+
+export class UserOutputMapper {
+  static toOutput(entity: UserEntity): UserOutput {
+    return entity.toJSON()
+  }
 }
