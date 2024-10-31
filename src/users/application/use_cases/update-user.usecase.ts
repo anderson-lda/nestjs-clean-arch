c031bbac857072ac654b72da81a86f4b2b7db463
anderson-lda <anderson.lima.araujo@hotmail.com>
2024-10-31 00:36:17 -0300

    added updateuserusecase

--- src/users/application/use_cases/update-user.usecase.ts
+++ src/users/application/use_cases/update-user.usecase.ts
@@ -1,10 +1,12 @@
 import { UserRepository } from "@/users/domain/repositories/user.repository"
 import { UserOutput, UserOutputMapper } from "../dto/user-output"
 import { UseCase as DefaultUseCase } from "@/shared/application/usecases/use-case"
+import { BadRequestError } from "@/shared/application/errors/bad-request-error"
 
-export namespace GetUserUseCase {
+export namespace UpdateUserUseCase {
   export type Input = {
     id: string
+    name: string
   }
 
   export type Output = UserOutput
@@ -15,7 +17,12 @@ export namespace GetUserUseCase {
     ){}
 
     async execute(input: Input): Promise<Output> {
-      const entity = await this.userRepository.findById(input.id)
+      if(!input.name) {
+        throw new BadRequestError('name not provided')
+      }
+      const entity = await this.userRepository.findById(input.id) //se o id não for válido, a exceção será lançada no lado do repositório
+      entity.update(input.name) //DDD: a entidade é alterada por ela mesma
+      await this.userRepository.update(entity)
       return UserOutputMapper.toOutput(entity)
     }
   }
