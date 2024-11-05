c4adb0e41421efa22bace3017d7c75b25da438c3
anderson-lda <anderson.lima.araujo@hotmail.com>
2024-11-05 14:34:40 -0300

    userscontroller unit tests - delete user  method

--- src/users/infrastructure/__tests__/unit/users.controller.spec.ts
+++ src/users/infrastructure/__tests__/unit/users.controller.spec.ts
@@ -90,4 +90,16 @@ describe('UsersController unit tests', () => {
     expect(output).toMatchObject(result);
     expect(mockUpdatePasswordUseCase.execute).toHaveBeenCalledWith({id,...input})
   });
+
+  it('should delete a user', async () => {
+    const output = undefined
+    const mockDeleteUserUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output))
+    }
+    sut['deleteUserUseCase'] = mockDeleteUserUseCase as any
+
+    const result = await sut.remove(id)
+    expect(output).toStrictEqual(result);
+    expect(mockDeleteUserUseCase.execute).toHaveBeenCalledWith({id})
+  });
 });
