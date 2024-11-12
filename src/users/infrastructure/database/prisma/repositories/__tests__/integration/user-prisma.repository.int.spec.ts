1e18d9b2cea9e1650458f1fec53ba98b26ccd0eb
anderson-lda <anderson.lima.araujo@hotmail.com>
2024-11-12 11:36:48 -0300

    integration tests of update method

--- src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int.spec.ts
+++ src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int.spec.ts
@@ -55,6 +55,22 @@ describe('UserPrismaRepository integration tests',()=>{
     })
   })
 
+  it('should throws error on update when entity not found',async ()=>{
+    const entity = new UserEntity(UserDataBuilder({}))
+
+    expect(()=>sut.update(entity)).rejects
+    .toThrow(new NotFoundError(`userModel not found using id ${entity._id}`))
+  })
+
+  it('should update an entity',async ()=>{
+    const entity = new UserEntity(UserDataBuilder({}))
+    const newUser = await prismaService.user.create({data: entity.toJSON()})
+    entity.update('new name')
+    await sut.update(entity)
+    const output = await prismaService.user.findUnique({where: { id: entity._id}})
+    expect(output.name).toBe('new name')
+  })
+
   describe('search method tests',()=>{
     it('should apply only pagination when  the others params are null', async ()=>{
       const createdAt = new Date()
