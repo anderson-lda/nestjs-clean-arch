428498d051bd727a9bf7102d7bb865b456c37d53
anderson-lda <anderson.lima.araujo@hotmail.com>
2024-11-21 14:10:19 -0300

    added usercollectionpresenter class

--- src/users/infrastructure/presenters/user.presenter.ts
+++ src/users/infrastructure/presenters/user.presenter.ts
@@ -1,4 +1,6 @@
+import { CollectionPresenter } from "@/shared/infrastructure/presenters/collection.presenter"
 import { UserOutput } from "@/users/application/dto/user-output"
+import { ListUsersUseCase } from "@/users/application/use_cases/listusers.usecase"
 import { Transform } from "class-transformer"
 
 export class UserPresenter {
@@ -13,5 +15,15 @@ export class UserPresenter {
     this.name = output.name
     this.email = output.email
     this.createdAt = output.createdAt
+  }
+}
+
+export class UserCollectionPresenter extends CollectionPresenter {
+  data: UserPresenter[]
+
+  constructor(output: ListUsersUseCase.Output){
+    const {items, ...paginationProps} = output //paginationProps é todo o resto fora items
+    super(paginationProps)
+    this.data = items.map(item => new UserPresenter(item))
   }
 }
