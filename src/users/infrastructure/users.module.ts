8de1a12a59f601baebc7fe648000664ac70aa0f9
anderson-lda <anderson.lima.araujo@hotmail.com>
2024-12-12 09:32:20 -0300

    implemented auth service

--- src/users/infrastructure/users.module.ts
+++ src/users/infrastructure/users.module.ts
@@ -12,8 +12,10 @@ import { UpdatePasswordUseCase } from '../application/use_cases/update-password.
 import { DeleteUserUseCase } from '../application/use_cases/delete-user.usecase';
 import { PrismaService } from '@/shared/infrastructure/database/prisma/prisma.service';
 import { UserPrismaRepository } from './database/prisma/repositories/user-prisma.repository';
+import { AuthModule } from '@/auth/infrastructure/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [UsersController],
   providers: [
     {
