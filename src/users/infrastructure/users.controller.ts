470cca810ec2f9eecbabbbe19d05b5e270092c05
anderson-lda <anderson.lima.araujo@hotmail.com>
2025-01-03 19:41:23 -0300

    added the documentation in login method

--- src/users/infrastructure/users.controller.ts
+++ src/users/infrastructure/users.controller.ts
@@ -66,6 +66,29 @@ export class UsersController {
     return UsersController.userToResponse(output)
   }
 
+  @ApiResponse({
+    status: 200,
+    schema: {
+      type: 'object',
+      properties: {
+        accessToken: {
+          type: 'string',
+        }
+      }
+    }
+  })
+  @ApiResponse({
+    status: 422,
+    description: 'Corpo da requisição com dados inválidos',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Email não encontrado',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Credenciais inválidas',
+  })
   @HttpCode(200)
   @Post('login')
   async login(@Body() signinDto: SigninDto) {
